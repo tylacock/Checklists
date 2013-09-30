e001ee528bf952d954b6666a8d9f46554a968989
Matthijs Hollemans <mail@hollance.com>
2013-09-30 10:03:43 +0200

    Initial Commit

---
+++ Checklists/ChecklistsViewController.h
@@ -0,0 +1,13 @@
+//
+//  ChecklistsViewController.h
+//  Checklists
+//
+//  Created by Matthijs on 30-09-13.
+//  Copyright (c) 2013 Razeware LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ChecklistsViewController : UIViewController
+
+@end
---
+++ Checklists/ChecklistsViewController.m
@@ -0,0 +1,29 @@
+//
+//  ChecklistsViewController.m
+//  Checklists
+//
+//  Created by Matthijs on 30-09-13.
+//  Copyright (c) 2013 Razeware LLC. All rights reserved.
+//
+
+#import "ChecklistsViewController.h"
+
+@interface ChecklistsViewController ()
+
+@end
+
+@implementation ChecklistsViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
